58dce5b083ab72a80d3a48c9f7c91eda7071e06f
alimiladun <ailmiladun5050@gmail.com>
2025-05-23 23:46:19 +0600

    Initial commit

---
+++ Prime_number.py
@@ -0,0 +1,18 @@
+Enter_Number = input("Enter a prime number: ")
+
+while True:
+
+    if Enter_Number.isdigit():
+        Enter_Number = int(Enter_Number)
+        
+        if Enter_Number % = 1:
+            print(f"{Enter_Number} is a prime number.")
+        else:
+            print(f"{Enter_Number} is not a prime number.")
+    else:
+        print("Please enter a valid number.")
+    
+
+    Enter_Number = input("Enter a prime number: ")
+    
+
